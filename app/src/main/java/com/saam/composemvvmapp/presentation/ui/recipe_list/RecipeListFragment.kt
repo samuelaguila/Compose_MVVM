9f3d79b1a13ed44b004e70dbc49fe7e938b80d2f
saam <samuel.aguila_cuba@outlook.com>
2020-12-26 10:54:33 -0500

    RecyclerView-Card

--- app/src/main/java/com/saam/composemvvmapp/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/saam/composemvvmapp/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.saam.composemvvmapp.R
+import com.saam.composemvvmapp.presentation.components.RecipeCard
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,7 +21,21 @@ class RecipeListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_recipe_list, container, false)
-    }
+        return ComposeView(requireContext()).apply {
+            setContent {
 
+                val items= viewModel.recipes.value
+
+                LazyColumn {
+                    itemsIndexed(
+                        items = items
+                    ){ index, item ->
+                        RecipeCard(
+                            recipe = item,
+                            onClick = {})
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/saam/composemvvmapp/presentation/ui/recipe_list/RecipeListViewModel.kt
+++ app/src/main/java/com/saam/composemvvmapp/presentation/ui/recipe_list/RecipeListViewModel.kt
@@ -1,8 +1,13 @@
 package com.saam.composemvvmapp.presentation.ui.recipe_list
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.saam.composemvvmapp.domain.model.Recipe
 import com.saam.composemvvmapp.repository.RecipeRepository
+import kotlinx.coroutines.launch
 import javax.inject.Named
 
 class RecipeListViewModel
@@ -11,4 +16,21 @@ constructor(
     private val recipeRepository: RecipeRepository,
     @Named("auth_token") private val token: String
 ): ViewModel() {
+
+    val recipes: MutableState<List<Recipe>> = mutableStateOf(ArrayList())
+
+    init {
+        newSearch()
+    }
+
+    fun newSearch(){
+        viewModelScope.launch {
+            val result= recipeRepository.search(
+                token= token,
+                page= 1,
+                query = "chicken"
+            )
+            recipes.value= result
+        }
+    }
 }
